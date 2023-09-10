84e5a686219f4a717541dc812454710145c581dd
OckJuWon0831 <ockjuwon99@naver.com>
2023-09-10 21:21:49 +0900

    first commit

---
+++ src/main/java/com/example/dbclustering/DbClusteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbclustering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbClusteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbClusteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbclustering/DbClusteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbclustering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbClusteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
